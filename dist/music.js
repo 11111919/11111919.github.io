5ceaa4f52677362c14b1292f9e800fab14661989
1903510017@qq.com <1903510017@qq.com>
2020-01-15 15:18:16 +0800

    Site updated: 2020-01-15 15:18:11

--- dist/music.js
+++ dist/music.js
@@ -6,19 +6,19 @@ const ap = new APlayer({
       {
         name: "最美的期待",
         artist: '南征北站',
-        url: 'http://m10.music.126.net/20200114213045/5a2df199f25b506a4204c7d4241da90c/ymusic/0b45/e639/b768/d0c07f504f04bfe73ae87b139b34dcc7.mp3',
+        url: 'https://sharefs.yun.kugou.com/202001151432/ac06bbc532d3f2ee34a82dd9286bfce5/G086/M03/03/08/NpQEAFs0wg-ABXKHADobrayvNKE764.mp3',
         cover: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2533827078,375027369&fm=26&gp=0.jpg',
       },
       {
         name: '透明',
         artist: '邓紫棋',
-        url: 'http://m10.music.126.net/20200114220342/a778bbe47fb0b3e94460251f214f572a/ymusic/0108/0758/015a/6bee1351c20219e537d86001cff74d4b.mp3',
+        url: 'https://sharefs.yun.kugou.com/202001151442/6d5eb86800c05ae6545b3db0d9e024db/G181/M02/0D/03/9Q0DAF4EuB-AP1QJADUQTFQ8ejY036.mp3',
         cover: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2533827078,375027369&fm=26&gp=0.jpg',
       },
       {
         name: '像风一样自由',
         artist: '许巍',
-        url: 'http://m10.music.126.net/20200114220401/2b13d0e76e7054fe5e50a55c7cbfd7df/ymusic/9755/631c/4fcc/9524fff997e8a4f41c6083ef242e3536.mp3',
+        url: 'https://sharefs.yun.kugou.com/202001151429/7e1485169db383ac6afa71c408281fa0/G114/M07/09/19/EocBAFnKcJ2AG_mVADXdYr1eddc992.mp3',
         cover: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2533827078,375027369&fm=26&gp=0.jpg',
       }
     ]
