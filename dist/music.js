dc03a41cb1668e725b220d949ce018c361907518
1903510017@qq.com <1903510017@qq.com>
2020-01-15 17:28:47 +0800

    Site updated: 2020-01-15 17:28:30

--- dist/music.js
+++ dist/music.js
@@ -1,25 +1,3 @@
-const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    fixed: true,
-    autoplay: true,
-    audio: [
-      {
-        name: "最美的期待",
-        artist: '南征北站',
-        url: 'https://sharefs.yun.kugou.com/202001151432/ac06bbc532d3f2ee34a82dd9286bfce5/G086/M03/03/08/NpQEAFs0wg-ABXKHADobrayvNKE764.mp3',
-        cover: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2533827078,375027369&fm=26&gp=0.jpg',
-      },
-      {
-        name: '透明',
-        artist: '邓紫棋',
-        url: 'https://sharefs.yun.kugou.com/202001151442/6d5eb86800c05ae6545b3db0d9e024db/G181/M02/0D/03/9Q0DAF4EuB-AP1QJADUQTFQ8ejY036.mp3',
-        cover: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2533827078,375027369&fm=26&gp=0.jpg',
-      },
-      {
-        name: '像风一样自由',
-        artist: '许巍',
-        url: 'https://sharefs.yun.kugou.com/202001151429/7e1485169db383ac6afa71c408281fa0/G114/M07/09/19/EocBAFnKcJ2AG_mVADXdYr1eddc992.mp3',
-        cover: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2533827078,375027369&fm=26&gp=0.jpg',
-      }
-    ]
-});
+// build time:Wed Jan 15 2020 17:28:18 GMT+0800 (GMT+08:00)
+const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:true,audio:[{name:"最美的期待",artist:"南征北站",url:"https://sharefs.yun.kugou.com/202001151432/ac06bbc532d3f2ee34a82dd9286bfce5/G086/M03/03/08/NpQEAFs0wg-ABXKHADobrayvNKE764.mp3",cover:"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2533827078,375027369&fm=26&gp=0.jpg"},{name:"透明",artist:"邓紫棋",url:"https://sharefs.yun.kugou.com/202001151442/6d5eb86800c05ae6545b3db0d9e024db/G181/M02/0D/03/9Q0DAF4EuB-AP1QJADUQTFQ8ejY036.mp3",cover:"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2533827078,375027369&fm=26&gp=0.jpg"},{name:"像风一样自由",artist:"许巍",url:"https://sharefs.yun.kugou.com/202001151429/7e1485169db383ac6afa71c408281fa0/G114/M07/09/19/EocBAFnKcJ2AG_mVADXdYr1eddc992.mp3",cover:"https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2533827078,375027369&fm=26&gp=0.jpg"}]});
+//rebuild by neat 
