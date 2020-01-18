39900b509723baf43a1eb922cb0c595b101ececd
1903510017@qq.com <1903510017@qq.com>
2020-01-18 15:21:53 +0800

    Site updated: 2020-01-18 15:21:40

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Fri Jan 17 2020 22:06:26 GMT+0800 (GMT+08:00)
+// build time:Sat Jan 18 2020 15:21:27 GMT+0800 (GMT+08:00)
 $(function(){var t=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:true,lrcType:3});$.getJSON("https://api.fczbl.vip/163/?type=playlist&id=2022437288",function(e){t.list.add(e)})});
 //rebuild by neat 
