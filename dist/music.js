6db4313bf36d9b4cec7f4e40230d09c0be3de651
1903510017@qq.com <1903510017@qq.com>
2020-02-14 21:01:44 +0800

    Site updated: 2020-02-14 21:01:25

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Thu Feb 13 2020 18:20:37 GMT+0800 (GMT+08:00)
+// build time:Fri Feb 14 2020 21:01:14 GMT+0800 (GMT+08:00)
 $(function(){var t=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:true,lrcType:3});$.getJSON("https://api.i-meto.com/meting/api?type=playlist&id=2022437288",function(e){t.list.add(e)})});
 //rebuild by neat 
